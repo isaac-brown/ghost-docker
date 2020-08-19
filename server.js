3a352dc884192525168245750e587d58bf39bede
Isaac Brown <isaac.brown@flexigroup.co.nz>
2020-08-20 09:01:25 +1200

    chore: try use dynamic port

--- server.js
+++ server.js
@@ -3,8 +3,8 @@
 const express = require('express');
 
 // Constants
-const PORT = 8080;
-const HOST = '0.0.0.0';
+const PORT = process.env.PORT || 8080;
+const HOST = process.env.HOST || '0.0.0.0';
 
 // App
 const app = express();
